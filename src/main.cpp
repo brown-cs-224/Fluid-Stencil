592affce22a7f00521c474dd30a248b6a6aca506
Arin1604 <arin_idhant@brown.edu>
2026-03-10 20:23:10 -0400

    basic imgui integration complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,36 @@
 #include <QSurfaceFormat>
 #include <QScreen>
 
+void run_imgui_test();
+
 int main(int argc, char *argv[])
 {
-    srand(static_cast<unsigned>(time(0)));
+    run_imgui_test();
+    // srand(static_cast<unsigned>(time(0)));
 
-    // Create a Qt application
-    QApplication a(argc, argv);
-    QCoreApplication::setApplicationName("Fluid Simulation");
-    QCoreApplication::setOrganizationName("CS 2240");
-    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+    // // Create a Qt application
+    // QApplication a(argc, argv);
+    // QCoreApplication::setApplicationName("Fluid Simulation");
+    // QCoreApplication::setOrganizationName("CS 2240");
+    // QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
-    // Set OpenGL version to 4.1 and context to Core
-    QSurfaceFormat fmt;
-    fmt.setVersion(4, 1);
-    fmt.setProfile(QSurfaceFormat::CoreProfile);
-    QSurfaceFormat::setDefaultFormat(fmt);
+    // // Set OpenGL version to 4.1 and context to Core
+    // QSurfaceFormat fmt;
+    // fmt.setVersion(4, 1);
+    // fmt.setProfile(QSurfaceFormat::CoreProfile);
+    // QSurfaceFormat::setDefaultFormat(fmt);
 
-    // Create a GUI window
-    MainWindow w;
-    w.resize(600, 500);
-    int desktopArea = QGuiApplication::primaryScreen()->size().width() *
-                      QGuiApplication::primaryScreen()->size().height();
-    int widgetArea = w.width() * w.height();
-    if (((float)widgetArea / (float)desktopArea) < 0.75f)
-        w.show();
-    else
-        w.showMaximized();
+    // // Create a GUI window
+    // MainWindow w;
+    // w.resize(600, 500);
+    // int desktopArea = QGuiApplication::primaryScreen()->size().width() *
+    //                   QGuiApplication::primaryScreen()->size().height();
+    // int widgetArea = w.width() * w.height();
+    // if (((float)widgetArea / (float)desktopArea) < 0.75f)
+    //     w.show();
+    // else
+    //     w.showMaximized();
 
 
-    return a.exec();
+    // return a.exec();
 }
